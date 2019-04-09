39bb118c7d1359b24072638fe51d57d3ba8156dd
Pauli <pjseoane@gmail.com>
2019-04-08 23:18:36 -0300

    book Analysis

--- Classes/cBookAnalysis.py
+++ Classes/cBookAnalysis.py
@@ -2,11 +2,11 @@
 # from Classes import cGetMarketData as md
 from Robots import cZrobot as zR
 
+
 class cBookAnalysis (zR.zRobot):
 
     def __init__(self, symbols):
         super().__init__(symbols)
-
 
     def getSUMContractsOpenOrders(self, ticker):
         contratos = 0
@@ -60,11 +60,11 @@ if __name__ == '__main__':
     obf = suscription.getOrdenesAll(suscription.account)
 
     # print("High Limit: ", ticker1, suscription.getContractHighLimit(ticker1))
-    print("Order Book All:", suscription.getOrdenesAll(suscription.account))
-    print("Order Book Open:", suscription.getOrdenesOpen(suscription.account))
-    print("Order Book Filled:", suscription.getOrdenesFilled(suscription.account))
-    # print("Filled Orders function", suscription.getFilledOrders(ticker1))
-    print("Open Contracts / Value", suscription.getSUMContractsOpenOrders(ticker1), suscription.getSUMValueOpenOrders(ticker1))
+    # print("Order Book All:", suscription.getOrdenesAll(suscription.account))
+    # print("Order Book Open:", suscription.getOrdenesOpen(suscription.account))
+    # print("Order Book Filled:", suscription.getOrdenesFilled(suscription.account))
+    # # print("Filled Orders function", suscription.getFilledOrders(ticker1))
+    print("Open Contracts / Value: ", ticker1, suscription.getSUMContractsOpenOrders(ticker1), suscription.getSUMValueOpenOrders(ticker1))
 
     # msg = simplejson.loads(message)
 else:
