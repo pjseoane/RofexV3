f1e26647a0911e0330c2f099b8daf76e66c9bac4
pseoane <pjseoane@gmail.com>
2019-05-29 15:32:32 -0300

    ccl ay24

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RofexV3',
+    version='1.0',
+    packages=['Robots', 'Classes', 'Scripts', 'Notebooks', 'Resources'],
+    url='',
+    license='',
+    author='pauli',
+    author_email='pjseoane@gmail.com',
+    description=''
+)
